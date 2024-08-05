63941f3311dfa3aa33d3d16d1caf61520f17ffc9
afyww <afyww18@gmail.com>
2024-08-06 00:39:33 +0700

    up

--- src/components/Home/Motion.jsx
+++ src/components/Home/Motion.jsx
@@ -3,16 +3,15 @@ function Motion() {
     <div className="grid grid-cols-1 h-screen bg-black">
       <div className="my-auto">
         <div className="video-container relative">
-          <div className="">
-            <iframe
-              id="player"
-              type="text/html"
-              height="768"
-              src="https://www.youtube.com/embed/2vbr502XSgM?playlist=2vbr502XSgM&autoplay=1&loop=1&controls=0&mute=0"
-              frameBorder="0"
-              className="w-screen h-screen"
-            ></iframe>
-          </div>
+          <iframe
+            id="player"
+            type="text/html"
+            height="768"
+            src="https://www.youtube.com/embed/2vbr502XSgM?autoplay=1&loop=1&playlist=2vbr502XSgM&controls=0&mute=0&modestbranding=1"
+            frameBorder="0"
+            className="w-screen h-screen"
+            allow="autoplay; encrypted-media"
+          ></iframe>
         </div>
       </div>
     </div>
