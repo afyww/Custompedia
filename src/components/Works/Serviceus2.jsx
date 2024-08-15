8ea69bc0674563f201bc9921b1ea1eb105ba8659
afyww <afyww18@gmail.com>
2024-08-15 21:51:59 +0700

    upp

--- src/components/Works/Serviceus2.jsx
+++ src/components/Works/Serviceus2.jsx
@@ -4,10 +4,10 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 
 const MediaCard = ({ src, title, projectId }) => (
-  <div className="bg-white rounded-lg overflow-hidden shadow-lg w-full sm:w-[calc(50%-1rem)] md:w-[calc(33.333%-1rem)] lg:w-[calc(25%-1rem)] mb-4">
+  <div className="bg-white rounded-lg overflow-hidden shadow-lg w-full sm:w-[calc(50%-1rem)] md:w-[calc(33.333%-1rem)] lg:w-[calc(25%-1rem)]">
     <div className="relative pb-[56.25%]">
       {" "}
-      {/* 16:9 aspect ratio */}
+
       <Link to={`/project/${projectId}`}>
         <img
           src={src}
@@ -15,9 +15,6 @@ const MediaCard = ({ src, title, projectId }) => (
           className="absolute top-0 left-0 w-full h-full object-cover"
         />
       </Link>
-    </div>
-    <div className="p-4">
-      <h3 className="font-bold text-lg">{title}</h3>
     </div>
   </div>
 );
@@ -103,8 +100,6 @@ const ServicesSection = () => {
               <MediaCard
                 key={project.id}
                 src={`https://custom.beilcoff.shop/storage/${project.img1}`}
-                title={project.project}
-                projectId={project.id}
               />
             ))}
           </div>
