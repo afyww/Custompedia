3ecfc7117b7bb7cdc4b1ac61174a79becf4e7ba9
afyww <afyww18@gmail.com>
2024-08-15 22:03:22 +0700

    upp

--- src/components/About/Aboutus.jsx
+++ src/components/About/Aboutus.jsx
@@ -8,11 +8,11 @@ const Aboutus = () => {
         alt="Custompedia Team" 
         className="object-cover opacity-50 w-full h-full absolute" 
       />
-      <div className="relative z-10 flex flex-col items-center justify-center text-center text-white">
+      <div className="relative text-center">
         <h1 className="text-4xl xl:text-8xl font-bold text-yellow-500">
           CUSTOMPEDIA
         </h1>
-        <p className="text-base xl:text-2xl font-extrabold xl:w-3/4">
+        <p className="text-base xl:text-2xl font-extrabold xl:w-3/4 text-white">
           A full-service marketing agency that creates integrated, end-to-end marketing experiences with a creative touch. We help you optimize the full customer journey.
         </p>
       </div>
