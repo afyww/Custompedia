0d2e12ef57417a7c56b709a2417befb3492c9af4
afyww <afyww18@gmail.com>
2024-08-16 13:00:01 +0700

    upp

--- src/components/Whatsapp.jsx
+++ src/components/Whatsapp.jsx
@@ -9,14 +9,14 @@ function Whatsapp() {
 
   return (
     <>
-      <div className="z-50 fixed bottom-6 left-4 opacity-85 shadow-blue-500 hover:opacity-100">
+      <div className="z-50 fixed bottom-8 left-6 opacity-85 shadow-blue-500 hover:opacity-100">
         <button onClick={scrollToTop}>
           <div className="bg-blue-600 opacity-85 shadow-blue-600 xl:p-2 rounded-xl px-2 py-6  hover:opacity-100">
             <FaArrowUp className="text-white xl:w-6 xl:h-20 w-4 h-8" />
           </div>
         </button>
       </div>
-      <div className="z-50 fixed bottom-12 right-4 opacity-85 shadow-emerald-200 hover:opacity-100">
+      <div className="z-50 fixed bottom-10 right-6 opacity-85 shadow-emerald-200 hover:opacity-100">
         <Link to="">
           <img src={WaLogo} alt="WhatsApp Logo" className="w-16 h-16 xl:w-20 xl:h-20" />
         </Link>
