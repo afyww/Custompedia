f7a02f0dd49d5815ea567a6ce88c2b14e64fb831
afyww <afyww18@gmail.com>
2024-08-16 17:22:54 +0700

    upp

--- src/components/About/Company.jsx
+++ src/components/About/Company.jsx
@@ -1,8 +1,8 @@
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import sample1 from '../../assets/images/sample1.jpg';
-import company from '../../assets/images/company.png'
+import sample1 from "../../assets/images/sample1.jpg";
+import company from "../../assets/images/company.png";
 
 const Company = () => {
   const settings = {
@@ -11,27 +11,62 @@ const Company = () => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    arrows: true
+    arrows: true,
+  };
+
+  const logoAnimationStyle = {
+    display: "flex",
+    whiteSpace: "nowrap",
+    position: "absolute",
+    animation: "scroll 20s linear infinite",
   };
 
   return (
     <section className="bg-black grid grid-cols-1">
-      <div className="p-10 my-auto space-y-16">
-        <div className='max-w-6xl mx-auto'>
-        <Slider {...settings}>
-          <div>
-            <img src={sample1} alt="Slide 1" className="w-full h-auto rounded-xl" />
-          </div>
-          <div>
-            <img src={sample1} alt="Slide 2" className="w-full h-auto rounded-xl" />
-          </div>
-          <div>
-            <img src={sample1} alt="Slide 3" className="w-full h-auto rounded-xl" />
-          </div>
-        </Slider>
+      <div className="xl:p-10  p-8 my-auto space-y-16">
+        <div className="max-w-6xl mx-auto">
+          <Slider {...settings}>
+            <div>
+              <img
+                src={sample1}
+                alt="Slide 1"
+                className="w-full h-auto rounded-xl"
+              />
+            </div>
+            <div>
+              <img
+                src={sample1}
+                alt="Slide 2"
+                className="w-full h-auto rounded-xl"
+              />
+            </div>
+            <div>
+              <img
+                src={sample1}
+                alt="Slide 3"
+                className="w-full h-auto rounded-xl"
+              />
+            </div>
+          </Slider>
         </div>
-        <div className="">
-          <img src={company} alt="Logos" className="mx-auto w-full" />
+        <style>
+          {`
+          @keyframes scroll {
+            0% {
+              transform: translateX(0);
+            }
+            100% {
+              transform: translateX(-100%);
+            }
+          }
+        `}
+        </style>
+        <div className="overflow-hidden relative h-16">
+          <div style={logoAnimationStyle}>
+            {/* Duplicate the logo image for seamless scrolling */}
+            <img src={company} alt="Logos" className="h-full" />
+            <img src={company} alt="Logos" className="h-full" />
+          </div>
         </div>
       </div>
     </section>
