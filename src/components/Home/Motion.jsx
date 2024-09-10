b15636d2231816dddf3e4a46e7ef5407b2fe4c7f
afyww <afyww18@gmail.com>
2024-09-10 14:16:00 +0700

    uppp

--- src/components/Home/Motion.jsx
+++ src/components/Home/Motion.jsx
@@ -1,22 +1,45 @@
+import React, { useEffect, useRef, useState } from 'react';
+
 function Motion() {
   const videoId = window.innerWidth <= 768 ? "G7eYjjLX4M0" : "2vbr502XSgM";
+  const [isVisible, setIsVisible] = useState(false);
+  const iframeRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsVisible(entry.isIntersecting);
+      },
+      { threshold: 0.5 } // Adjust threshold as needed
+    );
+
+    if (iframeRef.current) {
+      observer.observe(iframeRef.current);
+    }
+
+    return () => {
+      if (iframeRef.current) {
+        observer.unobserve(iframeRef.current);
+      }
+    };
+  }, []);
 
   return (
     <div className='h-screen grid grid-cols-1'>
       <div className="relative h-full w-full overflow-hidden">
         <iframe
+          ref={iframeRef}
           title="YouTube Video"
-          className="absolute top-0 left-0 w-full h-full"
+          className="top-0 left-0 w-full h-full"
           style={{
-            position: 'fixed',
             top: '0',
             left: '0',
             width: '100vw',
             height: '100vh',
-            zIndex: '-1', // Sends the video to the background, allowing other content to be displayed on top.
+            zIndex: '-1',
             pointerEvents: 'none',
           }}
-          src={`https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1&loop=1&playlist=${videoId}&controls=0&modestbranding=1&rel=0&showinfo=0&iv_load_policy=3`}
+          src={`https://www.youtube.com/embed/${videoId}?autoplay=${isVisible ? 1 : 0}&mute=1&loop=1&playlist=${videoId}&controls=0&modestbranding=1&rel=0&showinfo=0&iv_load_policy=3`}
           frameBorder="0"
           allow="autoplay; encrypted-media"
           allowFullScreen
